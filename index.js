4da543bbad0d90fc721eb74157a55f74879063f9
KWANGHEE <polyckh6092@gmail.com>
2020-12-21 11:43:29 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const port = 5000
 const bodyParser = require('body-parser')
 const {User} = require('./models/User')
 
+const config = require('./config/key')
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kwannee:256z1532@reactpractice.16uzj.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected...'))
 .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('되나요?')
 })
 
 app.post('/register',(req,res)=>{
